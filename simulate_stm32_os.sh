ab4176a1f07e7e6e694d0293704e5baf2a586fd6
Daniel Mårtensson <danielmartensson@github.com>
2024-09-22 14:25:12 +0200

    More clean up

--- simulate_stm32_os.sh
+++
@@ -1,7 +0,0 @@
-# Install Qemu for ARM
-sudo apt-get install qemu-system-arm -y
-
-# Simulate
-qemu-system-arm -M virt -cpu cortex-a7 -kernel zImage \
--initrd initrd.cpio.gz -dtb stm32mp151a-stm32-computer-firmware-mx.dtb \
--nographic -append "console=ttyAMA0"
