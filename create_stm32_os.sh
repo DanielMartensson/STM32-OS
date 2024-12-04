8685713f819403442fff5ed800bc5bf85d7c7628
Evildeeds <x_evil_deeds@hotmail.com>
2024-12-04 01:43:37 +0100

    Improved rootfs build scripts

--- create_stm32_os.sh
+++ create_stm32_os.sh
@@ -1,11 +1,16 @@
+#!/bin/sh
+
 # Remove old
 rm rootfs.ext4
 
 # Make file
-dd if=/dev/zero of=rootfs.ext4 bs=1M count=2100
+dd if=/dev/zero of=rootfs.ext4 bs=1M count=3000
 
 # Make filesystem
 mkfs.ext4 rootfs.ext4
+
+# Create folder
+sudo mkdir -p /mnt/rootfs
 
 # Mount
 sudo mount -o loop rootfs.ext4 /mnt/rootfs
@@ -13,11 +18,11 @@ sudo mount -o loop rootfs.ext4 /mnt/rootfs
 # Go inside
 cd rootfs
 
-# Create folder
-sudo mkdir /mnt/rootfs
-
 # Fill except /proc
 sudo cp -a bin home/ media/ opt/ root/ sbin usr/ boot/ etc/ lib mnt/ srv/ tmp/ var/ dev/ run/ sys/ /mnt/rootfs/
+
+# Restore file ownership and permissions
+sudo setfacl --restore=../rootfs.facl
 
 # Umount
 sudo umount /mnt/rootfs
@@ -27,9 +32,3 @@ cd ..
 
 # Copy
 cp rootfs.ext4 ../STM32-Computer/Firmware/STM32-Computer-Firmware/CA7/FIP_artifacts/rootfs.ext4
-
-
-
-
-
-
